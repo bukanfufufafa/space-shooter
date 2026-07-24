8dcca4383f74030cc591035963efafa1db5fe35e
Gavin <gavin.mahesya2006@gmail.com>
2026-07-24 22:24:15 +0700

    Skill Part 1

---
+++ Assets/Script/Player/PlayerSkillController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PlayerSkillController : MonoBehaviour
+{
+    [Header("Skill yang dipilih player (diisi lewat SetSkill saat pemilihan)")]
+    public ActiveSkillSO selectedSkill;
+
+    [Header("Referensi Laser (child dari firePoint, taruh manual di Inspector)")]
+    public LaserBeamController laserBeam;
+
+    private int usesLeft;
+
+    private void Start()
+    {
+        ResetUsesForNewStage();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryActivateSkill();
+        }
+    }
+
+    private void TryActivateSkill()
+    {
+        if (selectedSkill == null) return;
+
+        if (usesLeft <= 0)
+        {
+            Debug.Log("Skill sudah habis dipakai untuk stage ini!");
+            return;
+        }
+
+        selectedSkill.Activate(this);
+        usesLeft--;
+        Debug.Log($"{selectedSkill.skillName} dipakai. Sisa pemakaian: {usesLeft}");
+    }
+
+    public void ResetUsesForNewStage()
+    {
+        if (selectedSkill != null)
+            usesLeft = selectedSkill.maxUsesPerStage;
+    }
+
+    public void SetSkill(ActiveSkillSO skill)
+    {
+        selectedSkill = skill;
+        ResetUsesForNewStage();
+    }
+
+    public int GetUsesLeft() => usesLeft;
+}
