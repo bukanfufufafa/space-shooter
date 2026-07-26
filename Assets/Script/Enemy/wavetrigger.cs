a93eaf2f04f4bbaff739c1f202aa58fbe9fc9e9f
Gavin <gavin.mahesya2006@gmail.com>
2026-07-26 10:12:36 +0700

    Slight Push

--- Assets/Script/Enemy/wavetrigger.cs
+++ Assets/Script/Enemy/wavetrigger.cs
@@ -1,47 +1,64 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class wavetrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     [SerializeField] private List<EnemySpawner> spawnerWave1 = new List<EnemySpawner>();
     [SerializeField] private List<EnemySpawner> spawnerWave2 = new List<EnemySpawner>();
 
-    private int paternDefeated;
-    void Start()
+    [Header("Referensi")]
+    [SerializeField] private StageManager stageManager;
+
+    private bool wave2Started = false;
+    private bool waveAllDone = false;
+
+    private void Start()
     {
-        
         foreach (var item in spawnerWave2)
-        {
             item.gameObject.SetActive(false);
-        }
 
         foreach (var item in spawnerWave1)
-        {
             item.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (waveAllDone) return; // udah kelar semua, gak perlu ngecek lagi
+
+        if (!wave2Started)
+        {
+            // Cek pakai == (perbandingan), bukan = (assignment)
+            bool wave1Done = spawnerWave1.All(s => s.defeated == true);
+
+            if (wave1Done)
+            {
+                StartWave2();
+            }
+        }
+        else
+        {
+            bool wave2Done = spawnerWave2.All(s => s.defeated == true);
+
+            if (wave2Done)
+            {
+                waveAllDone = true;
+
+                if (stageManager != null)
+                    stageManager.OnStageCleared();
+
+                Debug.Log("Semua wave di stage ini selesai!");
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartWave2()
     {
-        foreach (EnemySpawner spawner in spawnerWave1)
-        {
-            if (spawner.defeated = true)
-            {
-                paternDefeated++;
-            }
-        }
+        wave2Started = true;
 
-        foreach (EnemySpawner spawner in spawnerWave2)
-        {
-            if (spawner.defeated = true)
-            {
-                paternDefeated++;
-            }
-        }
+        foreach (var item in spawnerWave2)
+            item.gameObject.SetActive(true);
+
+        Debug.Log("Wave 1 selesai, Wave 2 dimulai!");
     }
 }
--- Assets/Script/Manager/StageManager.cs
+++ Assets/Script/Manager/StageManager.cs
@@ -5,8 +5,12 @@ public enum GameStage { Stage1, Stage2, Stage3, Done }
 public class StageManager : MonoBehaviour
 {
     [Header("Panel UI (drag dari Hierarchy)")]
-    public GameObject activeSkillPanel;  
-    public GameObject passiveSkillPanel;  
+    public GameObject activeSkillPanel; 
+    public GameObject passiveSkillPanel; 
+
+    [Header("Wave Stage 2 & 3 (drag GameObject yang ada wavetrigger-nya)")]
+    public GameObject stage2Waves; 
+    public GameObject stage3Waves; 
 
     public GameStage currentStage = GameStage.Stage1;
 
@@ -25,33 +29,45 @@ public class StageManager : MonoBehaviour
             case GameStage.Stage3:
                 Debug.Log("Semua stage selesai!");
                 currentStage = GameStage.Done;
+                // lanjut ke scene menang / ending, dll
                 break;
         }
     }
 
     private void ShowPanel(GameObject panel)
     {
-        Time.timeScale = 0f;
+        Time.timeScale = 0f; // pause game selagi player milih (opsional, boleh dihapus)
         panel.SetActive(true);
     }
+
     public void AdvanceToNextStage()
     {
         Time.timeScale = 1f;
 
         switch (currentStage)
         {
-            case GameStage.Stage1: currentStage = GameStage.Stage2; break;
-            case GameStage.Stage2: currentStage = GameStage.Stage3; break;
+            case GameStage.Stage1:
+                currentStage = GameStage.Stage2;
+                if (stage2Waves != null) stage2Waves.SetActive(true);
+                break;
+
+            case GameStage.Stage2:
+                currentStage = GameStage.Stage3;
+                if (stage3Waves != null) stage3Waves.SetActive(true);
+                break;
         }
 
         Debug.Log($"Lanjut ke {currentStage}");
     }
-    
+
     public void ResetStage()
     {
         currentStage = GameStage.Stage1;
         Time.timeScale = 1f;
         activeSkillPanel.SetActive(false);
         passiveSkillPanel.SetActive(false);
+
+        if (stage2Waves != null) stage2Waves.SetActive(false);
+        if (stage3Waves != null) stage3Waves.SetActive(false);
     }
 }
